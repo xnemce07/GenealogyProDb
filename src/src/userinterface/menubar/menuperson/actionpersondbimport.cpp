a4837fbf14e3d2e9fea3c9ec200ace17ff1ae38e
Leopold Nemček <leo.nemcek@gmail.com>
2022-12-20 17:28:30 +0100

    Basic data loading

--- src/src/userinterface/menubar/menuperson/actionpersondbimport.cpp
+++ src/src/userinterface/menubar/menuperson/actionpersondbimport.cpp
@@ -25,36 +25,94 @@ void ActionPersonDbImport::process()
 void ActionPersonDbImport::modifyPerson()
 {
 
+    int id = 1;
 
     QString dbHostname = "localhost";
     QString dbName = "demos";
     QString dbUsername = "genealogy";
     QString dbPassword = "Genealogy49!";
 
-//    bool ok = true;
-
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    bool drvrAvailavle = db.isDriverAvailable("QMYSQL");
     db.setHostName(dbHostname);
     db.setDatabaseName(dbName);
     db.setUserName(dbUsername);
     db.setPassword(dbPassword);
 
-    bool ok = db.open();
+    if(db.open()){
+        QSqlQuery givenNameQuery;
+        givenNameQuery.exec(QString("SELECT n.name, pn.name_order "
+                           "FROM person p "
+                           "LEFT JOIN person_name pn on p.id = pn.person_id "
+                           "LEFT JOIN name n on n.id = pn.name_id "
+                           "WHERE p.id = %1 "
+                           "ORDER BY pn.name_order ASC").arg(id));
+        QString givenName = "";
+        while (givenNameQuery.next()) {
+            givenName += givenNameQuery.value(0).toString();
+        }
+        _kernel.record()->setIndividualNameGiven(_newPerson,givenName);
 
-    QSqlError err;
-    if(ok){
-       _kernel.record()->setIndividualNameGiven(_newPerson, "Mark");
-    } else {
-        _kernel.record()->setIndividualNameGiven(_newPerson, "Fred");
-        err = db.lastError();
+        QSqlQuery surnameQuery;
+        surnameQuery.exec(QString("SELECT s.name, nS.name, p.religion "
+                                  "FROM person p "
+                                  "LEFT JOIN surname s on p.sname = s.id "
+                                  "LEFT JOIN normalizedSurname nS on s.norm_name_id = nS.id "
+                                  "WHERE p.id = %1").arg(id));
+
+        surnameQuery.next();
+        if(surnameQuery.value(1).toString() != ""){
+            _kernel.record()->setIndividualNameSurname(_newPerson,surnameQuery.value(1).toString());
+        } else if(surnameQuery.value(0).toString() != ""){
+            _kernel.record()->setIndividualNameSurname(_newPerson,surnameQuery.value(0).toString());
+        }
+
+        QSqlQuery deathQuery;
+        deathQuery.exec(QString("SELECT b.death_place, b.dead_date, b.dead_time, b.burial_place, b.burial_date, b.comment, bDC.name, nDC.name "
+                                "FROM person p "
+                                "LEFT JOIN burial b on p.burial_id = b.id "
+                                "LEFT JOIN burialDeathCause bDC on b.death_cause = bDC.id "
+                                "LEFT JOIN normalizedDeathCause nDC on bDC.norm_name_id = nDC.id "
+                                "WHERE p.id = %1").arg(id));
+        deathQuery.next();
+        if(deathQuery.value(0).toString() != ""){
+            _kernel.record()->setIndividualDeathPlaceName(_newPerson,deathQuery.value(0).toString());
+        }
+        if(deathQuery.value(1).toString() != ""){
+            _kernel.record()->setIndividualDeathDate1(_newPerson,QDate::fromString(deathQuery.value(1).toString(),"yyyy-MM-dd"));
+        }
+        if(deathQuery.value(3).toString() != ""){
+            _kernel.record()->setIndividualBurialPlaceName(_newPerson,deathQuery.value(3).toString());
+        }
+        if(deathQuery.value(4).toString() != ""){
+            _kernel.record()->setIndividualBurialDate1(_newPerson,QDate::fromString(deathQuery.value(4).toString(),"yyyy-MM-dd"));
+        }
+        if(deathQuery.value(7).toString() != ""){
+            _kernel.record()->setIndividualDeathCause(_newPerson,deathQuery.value(7).toString());
+        } else if(deathQuery.value(6).toString() != ""){
+            _kernel.record()->setIndividualDeathCause(_newPerson,deathQuery.value(6).toString());
+        }
+
+        QSqlQuery birthQuery;
+        birthQuery.exec( QString("SELECT b.birth_date, b.birth_time, b.baptism_date, b.baptism_time, b.sex "
+                                 "FROM person p "
+                                 "LEFT JOIN birth b on p.birth_id = b.id "
+                                 "WHERE p.id = %1").arg(1));
+        birthQuery.next();
+        if(birthQuery.value(0).toString() != ""){
+           _kernel.record()->setIndividualBirthDate1(_newPerson,QDate::fromString(birthQuery.value(0).toString(),"yyyy-MM-dd"));
+        }
+        if(birthQuery.value(2).toString() != ""){
+            _kernel.record()->setIndividualChristeningDate1(_newPerson,QDate::fromString(birthQuery.value(2).toString()));
+        }
+        if(birthQuery.value(4).toString() == "M"){
+            _kernel.record()->setIndividualGender(_newPerson,Gender::MALE);
+        } else if(birthQuery.value(4).toString() == "F"){
+            _kernel.record()->setIndividualGender(_newPerson,Gender::FEMALE);
+        }
+
+
     }
-    bool valid = db.isValid();
 
-    auto errortType = err.type();
-    auto errortext = err.text();
-
-    auto i = 5;
     return ;
 }
 
